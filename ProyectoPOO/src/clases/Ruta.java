4570ea95b63560754bd28f24b51b926c04315d21
joseph <jlmoreno@espol.edu.ec>
2023-11-07 08:20:03 -0500

    Creacion de clase ruta

---
+++ ProyectoPOO/src/clases/Ruta.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author jlmor
+ */
+public class Ruta {
+    private String origen;
+    private String destino;
+    
+
+public Ruta(String origen, String destino){
+    this.origen=origen;
+    this.destino=destino;
+    }
+
+public String getOrigen(){
+    return origen;
+    }
+
+public void setOrigen(String origen){
+    this.origen = origen;
+}
+
+public String getDestino(){
+    return destino;
+}
+public void setDestino(String destino){
+    this.destino=destino;
+}
+
+public void ruta(String origen,String destino){
+    
+    }
+public String toString(){
+    return("Origen: " + this.origen + " - Destino: " + this.destino );
+  }
+}
